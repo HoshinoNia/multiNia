24949ed3266a0b3aad8d1d8ead29e84ed24cf1cc
HoshinoNia <70749961+HoshinoNia@users.noreply.github.com>
2020-10-05 11:17:50 +0900

    ipcRenderer周りの実装

---
+++ index_old.js
@@ -0,0 +1,79 @@
+// const log = require('electron-log');
+// let windowArr = []
+// const liveUrl = "https://www.youtube.com/results?search_query=%E2%80%9D%E6%98%9F%E9%87%8E%E3%83%8B%E3%82%A2%E2%80%9D+&sp=EgJAAQ%253D%253D";
+// const button = document.querySelector('.btn-yt');
+
+// button.addEventListener('click', function (clickEvent) {
+//     log.info("ボタン押された");
+//     // windowArr.join(createWindow());
+// })
+
+console.log("index.js loaded")
+// var id = "btn-test"
+var btn = document.querySelector('.btn-test');
+btn.addEventListener("click", function (clickEvent) {
+    console.log("clicked");
+})
+// btn.onClick = function () {
+//     console.log("clicked");
+// }
+
+
+
+
+
+//   var windowArr = [];
+//   var liveUrl = "https://www.youtube.com/results?search_query=%E2%80%9D%E6%98%9F%E9%87%8E%E3%83%8B%E3%82%A2%E2%80%9D+&sp=EgJAAQ%253D%253D";
+//   var remote = require('remote');
+//   var BrowserWindow = remote.require('browser-window');
+
+//   var btn_yt = document.getElementById("btn-yt");
+//   // var btn_wrap = document.getElementById("btn-wrap");
+
+//   btn_yt.addEventListener('click', function (clickEvent) {
+//       // windowArr.join( createWindow() );
+//       // createWindow();
+//       remote.app.quit();
+//   })
+//   createWindow();
+
+function createWindow() {
+    // winを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
+    let win;
+    win = new BrowserWindow({
+        useContentSize: true,
+        width: 480,
+        height: 270,
+        minWidth: 100,
+        minHeight: 100,
+        resizable: true,
+        fullscreenable: false,
+        // frame: false,
+        alwaysOnTop: true,
+        // "title-bar-style": "hiddenInset",
+        // titleBarStyle: 'hiddenInset',
+        // "title-bar-style": "hidden-inset",
+        webPreferences: {
+            nodeIntegration: true
+        }
+        // 'node-integration': false
+    });
+    // win.setMenu(null);
+    win.menuBarVisible = false;
+
+    // // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
+    // win.loadURL('file://' + __dirname + '/index.html');
+
+    // ChromiumのDevツールを開く
+    // win.webContents.openDevTools();
+
+    // Load a remote URL
+    win.loadURL(liveUrl);
+
+    win.on('closed', function () {
+        win = null;
+        // app.quit();
+    });
+}
+//       windowArr.join(win);
+//       // return win;
--- main.js
+++ main.js
@@ -1,40 +1,73 @@
 // const $ = require('jquery');
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+// const electron = require('electron');
+// const app = electron.app;
+// const BrowserWindow = electron.BrowserWindow;
+"use strict";
+const {
+    app,
+    BrowserWindow,
+    ipcMain
+} = require('electron')
 
 // let win = null;
 const liveUrl = "https://www.youtube.com/results?search_query=%E2%80%9D%E6%98%9F%E9%87%8E%E3%83%8B%E3%82%A2%E2%80%9D+&sp=EgJAAQ%253D%253D";
 let w = 480;
 let h = 270;
 
-let mainWindow;
+let mainWindow, backWindow;
 let windowArr = []
-const url = "index.html"
+// const url = "index.html"
 
 app.on('ready', () => {
 
     mainWindow = new BrowserWindow({
-        "frame": false, //タイトルバー削除
+        // "frame": false, //タイトルバー削除
+        // transparent: false,
         'overlay-scrollbars': false,
         "title-bar-style": "hidden",
         'always-on-top': true,
         // transparent: true,
-        width: 180,
-        height: 80,
+        // width: 180,
+        // height: 80,
+        width: w,
+        height: h,
         webPreferences: {
-            nodeIntegration: false
+            // worldSafeExecuteJavaScript: true, // In Electron 12, the default will be changed to true.
+            nodeIntegration: true,
+            // nodeIntegration: false, // XSS対策としてnodeモジュールをレンダラープロセスで使えなくする
+            // contextIsolation: true, // レンダラープロセスに公開するAPIのファイル
         }
     });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     // mainWindow.loadURL(url)
     mainWindow.on('closed', function () {
         mainWindow = null;
-        app.quit()
+        app.quit();
     });
+
+    // menubar hidden
+    mainWindow.setMenu(null);
 
     // 下記のコメントを解除すると、デベロッパツールが起動する
     mainWindow.webContents.openDevTools();
+
+    // const backWindow = new BrowserWindow({
+    //     // "frame": false, //タイトルバー削除
+    //     // transparent: false,
+    //     'overlay-scrollbars': false,
+    //     "title-bar-style": "hidden",
+    //     'always-on-top': true,
+    //     // transparent: true,
+    //     // width: 180,
+    //     // height: 80,
+    //     width: w * 3,
+    //     height: h * 3,
+    //     webPreferences: {
+    //         nodeIntegration: true
+    //     }
+    // });
+    // backWindow.loadURL('file://' + __dirname + '/back.html');
+    // backWindow.setMenu(null);
 
     // const btn_yt = mainWindow.querySelector('.btn-youtube');
 
@@ -50,6 +83,11 @@ app.on('ready', () => {
     //     win = null;
     //     app.quit();
     // });
+    ipcMain.on('asynchronous-message', (event, arg) => { // channel名は「asynchronous-message」
+        console.log(arg) // "ping"を表示
+        windowArr.join(createWindow());
+        // event.reply('asynchronous-reply', 'pong')
+    })
 
 });
 
@@ -63,6 +101,8 @@ function createWindow() {
     // winを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
     let win;
     win = new BrowserWindow({
+        // parent: backWindow,
+        // modal: true,
         useContentSize: true,
         width: 480,
         height: 270,
